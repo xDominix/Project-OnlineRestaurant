aec6eb6a24b6aa4d32ca6598617043cbc46d598c
Your sisi <92535930+xDominix@users.noreply.github.com>
2022-11-07 19:35:02 +0100

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyD5JYU7WaZcaT-eMMHAzPSHphX55HOGeAU",
+    authDomain: "restaurantproject-b7c6e.firebaseapp.com",
+    projectId: "restaurantproject-b7c6e",
+    storageBucket: "restaurantproject-b7c6e.appspot.com",
+    messagingSenderId: "253430629907",
+    appId: "1:253430629907:web:d335c7853b0ebed6c7e1e6",
+    measurementId: "G-Q6C5KMW90J"
+  }
+};
---
+++ environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyD5JYU7WaZcaT-eMMHAzPSHphX55HOGeAU",
+    authDomain: "restaurantproject-b7c6e.firebaseapp.com",
+    projectId: "restaurantproject-b7c6e",
+    storageBucket: "restaurantproject-b7c6e.appspot.com",
+    messagingSenderId: "253430629907",
+    appId: "1:253430629907:web:d335c7853b0ebed6c7e1e6",
+    measurementId: "G-Q6C5KMW90J"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
